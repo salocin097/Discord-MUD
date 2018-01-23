e4d59c993594d21998a554fff189f150a6cb2776
Nicolas Liu <ndliu097@gmail.com>
2018-01-23 09:58:48 -0700

    Tile Class
    Instead of rewriting one for every single generator I made this. Will make a rendering class soon just to draw up the maps.

---
+++ Tile.py
@@ -0,0 +1,31 @@
+class Tile:
+    def __init__(self, walkable, transparent):
+        self.walkable = walkable
+
+        # by default, if a tile is walkable it is also transparent
+        if transparent is None:
+            transparent = walkable
+        self.transparent = transparent
+
+        # Transparent refers to whether or not it blocks LoS (Line of Sight)
+
+    # These may become enums in the future where we have set_water() make self.walkable = SWIMMABLE or
+    # something similar. For now my maze gens will just use this class and the first two methods.
+    # In general, walls will be printed as '#' and floors as '.'. Obviously this won't matter later
+    # but it's a placeholder.
+
+    def set_wall(self):
+        self.walkable = False
+        self.transparent = False
+
+    def set_floor(self):
+        self.walkable = True
+        self.transparent = True
+
+    def set_water(self):
+        self.walkable = False
+        self.transparent = True
+
+    def set_tall_grass(self):
+        self.walkable = True
+        self.transparent = False
