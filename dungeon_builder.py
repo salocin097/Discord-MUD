9b034b5b17944683fecccfc597d229e80cfcd812
Nicolas Liu <ndliu097@gmail.com>
2018-02-02 00:43:37 -0700

    Starting work on the rooms

--- dungeon_builder.py
+++ dungeon_builder.py
@@ -1,11 +1,12 @@
 from stage_builder import StageBuilder
 from tile import TILE_WALL, TILE_FLOOR
+from rectangle import Rect
 
 
 class DungeonBuilder(StageBuilder):
 
     def __init__(self):
-        self.num_room_attempts = 0
+        self.num_room_attempts = 20
         self.room_size_variance = 0
         self.extra_connector_chance = 0
 
@@ -29,6 +30,16 @@ class DungeonBuilder(StageBuilder):
         # fill the stage with walls
         self.stage.layout.fill_area(self.stage.width, self.stage.height, TILE_WALL)
 
-
+        self.generate_rectangles()
 
         raise NotImplementedError("Not done yet")
+
+    def generate_rectangles(self):
+
+        # this made need to belong to the whole DungeonBuilder class? not sure. generate_map may need it
+        rooms = []
+        room_counter = 0
+        for i in range(self.num_room_attempts):
+
+
+        raise NotImplementedError("WIP")
---
+++ rectangle.py
@@ -0,0 +1,18 @@
+class Rect:
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+    def center(self):
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return center_x, center_y
+
+    # may have touching walls?? may need to add +1 somewhere
+    def does_intersect(self, other):
+        # returns true if the rectangle intersects with the other rectangle
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+
